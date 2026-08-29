7d0719e71d7af91d5c7360eb8c89a40f4941c788
soenneker <jake@soenneker.com>
2026-08-29 14:17:03 -0500

    Improve public API XML documentation

--- src/Soenneker.Firecrawl.HttpClients/Abstract/IFirecrawlOpenApiHttpClient.cs
+++ src/Soenneker.Firecrawl.HttpClients/Abstract/IFirecrawlOpenApiHttpClient.cs
@@ -11,9 +11,9 @@ namespace Soenneker.Firecrawl.HttpClients.Abstract;
 public interface IFirecrawlOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
     /// <summary>
-    /// Gets the value.
+    /// Returns the configured http Client used by the firecrawl open api http client.
     /// </summary>
-    /// <param name="cancellationToken">The cancellation token.</param>
-    /// <returns>A task containing the result of the operation.</returns>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested http Client.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.Firecrawl.HttpClients/Registrars/FirecrawlOpenApiHttpClientRegistrar.cs
+++ src/Soenneker.Firecrawl.HttpClients/Registrars/FirecrawlOpenApiHttpClientRegistrar.cs
@@ -13,6 +13,8 @@ public static class FirecrawlOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="FirecrawlOpenApiHttpClient"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddFirecrawlOpenApiHttpClientAsSingleton(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
@@ -24,6 +26,8 @@ public static class FirecrawlOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="FirecrawlOpenApiHttpClient"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddFirecrawlOpenApiHttpClientAsScoped(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
