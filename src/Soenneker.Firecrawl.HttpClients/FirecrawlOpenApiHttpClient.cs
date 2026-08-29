7958af2fe0228fa439cbc89a898fdb82c5b915d1
soenneker <jake@soenneker.com>
2026-08-29 14:50:39 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Firecrawl.HttpClients/FirecrawlOpenApiHttpClient.cs
+++ src/Soenneker.Firecrawl.HttpClients/FirecrawlOpenApiHttpClient.cs
@@ -11,7 +11,7 @@ using Soenneker.Utils.HttpClientCache.Abstract;
 
 namespace Soenneker.Firecrawl.HttpClients;
 
-///<inheritdoc cref="IFirecrawlOpenApiHttpClient"/>
+/// <inheritdoc cref="IFirecrawlOpenApiHttpClient"/>
 public sealed class FirecrawlOpenApiHttpClient : IFirecrawlOpenApiHttpClient
 {
     private readonly IHttpClientCache _httpClientCache;
